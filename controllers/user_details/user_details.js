568fa145a307bb13cc1bb2d13fa0aef1d394e578
ivine <ivinejojuisac@gmail.com>
2024-05-25 01:14:42 +0530

    changes

--- controllers/user_details/user_details.js
+++ controllers/user_details/user_details.js
@@ -4,15 +4,16 @@ const admin = require('firebase-admin');
 const firestore = require('../../db'); // Assuming connection setup elsewhere
 
 const usersCollection = firestore.collection('users');
-
+const statusCollection = firestore.collection('docstatus');
 const getUserDetails = async (req, res) => {
   try {
     const userId = req.params.userId; // Assuming userId is passed in the URL parameters
 
     const snapshot = await usersCollection.get();
-
+    const statusSnapshot = await statusCollection.get();
     let userData = null;
-    
+    let userData2 = null;
+    let mergedData = null;
     snapshot.forEach(doc => {
       const data = doc.data();
       if (data.uid === userId) {
@@ -20,14 +21,24 @@ const getUserDetails = async (req, res) => {
         return;
       }
     });
+    statusSnapshot.forEach(doc => {
+      userData2 = doc.data();
+      if (userData2.uid === userId){
+        mergedData = { ...userData, ...userData2 };
+      }
+    });
+    
 
     if (!userData) {
       return res.status(404).send({ message: 'User not found' });
     }
+    // if (!userData2) {
+    //   return res.status(404).send({ message: 'Data not found' });
+    // }
 
     res.send({
       success: true,
-      data: userData,
+      data: mergedData
     });
   } catch (error) {
     console.error(error);
