96320546963969389f656706957c455cc69d9dff
ivine <ivinejojuisac@gmail.com>
2024-06-28 21:18:37 +0530

    change

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 
+
+
 // const register = require('../controllers/register/register.js');
 // const upload = require('../controllers/upload/upload.js');
 // const updatereg = require('../controllers/updatereg/updatereg.js');
@@ -20,47 +22,57 @@
 // const getfare = require('../controllers/customer_info/getfare.js');
 // const alltrip = require('../controllers/admin/alltrip.js');
 // const getfilter = require('../controllers/admin/getfilter.js');
-// const deleted = require('../controllers/admin/delete.js');
+// // const deleted = require('../controllers/admin/delete.js');
 // const getexcel = require('../controllers/admin/getreport.js');
 
-// const cors = require('cors')
+// const cors = require('cors');
+
+// // Define the allowed frontend origin
+// const frontendOrigin = 'http://3.91.86.142:80'; // Replace with the actual frontend IP or domain
+
+// // CORS options
+// const corsOptions = {
+//   origin: frontendOrigin,
+//   optionsSuccessStatus: 200
+// };
+
 // // const admin_login = require('../controllers/admin/adminlog.js');
 // module.exports = function(app) {
-  
+
 //     //driver registration
 //     app.post('/register', register.register);
 //     app.put('/updateregister', updatereg.updateUserDetails);
 //     app.post('/upload', upload.uploadFiles);
 //     app.post('/poststatus', poststatus.status);
-//     app.put('/updatestatus',cors(), updatestatus.updateStatus);
+//     app.put('/updatestatus', cors(corsOptions), updatestatus.updateStatus);
+
 //     //customer data added and updated
 //     app.post('/customerinfo', customer_info.customer_info);
 //     app.put('/customerupdate', customer_update.updateCustomer);
 
 //     //admin accnt creation
 //     app.post('/adminpost', admin_post.register);
-//     app.get('/getebills', cors(), ebill.getbill);
-//     app.get('/getexcel', cors(), getexcel.generateExcel);
-//     app.get('/getdriverrev', cors(), getdriverrev.getdriverrev);
-//     app.get('/getloc', cors(), getloc.getloc);
+//     app.get('/getebills', cors(corsOptions), ebill.getbill);
+//     app.get('/getexcel', cors(corsOptions), getexcel.generateExcel);
+//     app.get('/getdriverrev', cors(corsOptions), getdriverrev.getdriverrev);
+//     app.get('/getloc', cors(corsOptions), getloc.getloc);
 //     app.get('/alltrip/:userId', alltrip.getalltrips);
 //     app.get('/getfilter', getfilter.getfilter);
-    
+
 //     app.get('/trip/:userId', tripfilter.gettripfilter);
 //     // app.get('/trip/:userId', trip.getTripDetails );
-//     app.get('/userdetails/:userId', user_details.getUserDetails)
+//     app.get('/userdetails/:userId', user_details.getUserDetails);
 
 //     //admin
-//     app.get('/drivers',cors(), alldriver.getUserDetails)
+//     app.get('/drivers', cors(corsOptions), alldriver.getUserDetails);
 
-//     app.post('/postlive', postlive.postlive)
-//     app.get('/getlive/:userId', getlive.getlive)
+//     app.post('/postlive', postlive.postlive);
+//     app.get('/getlive/:userId', getlive.getlive);
 
-//     app.post('/postfares', postfare.postfare)
-//     app.get('/getfares', getfare.getfare)
+//     app.post('/postfares', postfare.postfare);
+//     app.get('/getfares', getfare.getfare);
 
-
-//     app.delete('/delete', deleted.deleteUsers);
+//     // app.delete('/delete', deleted.deleteUsers);
 // };
 
 
@@ -85,7 +97,7 @@ const postfare = require('../controllers/customer_info/postfare.js');
 const getfare = require('../controllers/customer_info/getfare.js');
 const alltrip = require('../controllers/admin/alltrip.js');
 const getfilter = require('../controllers/admin/getfilter.js');
-const deleted = require('../controllers/admin/delete.js');
+// const deleted = require('../controllers/admin/delete.js');
 const getexcel = require('../controllers/admin/getreport.js');
 
 const cors = require('cors');
@@ -96,7 +108,8 @@ const frontendOrigin = 'http://3.91.86.142:80'; // Replace with the actual front
 // CORS options
 const corsOptions = {
   origin: frontendOrigin,
-  optionsSuccessStatus: 200
+  methods: ['GET', 'POST', 'PUT'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
 };
 
 // const admin_login = require('../controllers/admin/adminlog.js');
@@ -135,5 +148,6 @@ module.exports = function(app) {
     app.post('/postfares', postfare.postfare);
     app.get('/getfares', getfare.getfare);
 
-    app.delete('/delete', deleted.deleteUsers);
+    // app.delete('/delete', deleted.deleteUsers);
 };
+
